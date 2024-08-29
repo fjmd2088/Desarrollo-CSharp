a631588a2dc3af3ffbd26fcf4f4314f11692e321
fjmd2088 <fjmartinezdeferia@gmail.com>
2024-08-29 16:17:48 -0600

    Proyecto inicial

---
+++ Desarrollo_C#/Desarrollo_C#/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Desarrollo_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nombre_ua = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empleo_anterior = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtRutaDirectorio = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 72);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1219, 624);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1211, 598);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(54, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1117, 444);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre_ua,
+            this.empleo_anterior});
+            this.dataGridView1.Location = new System.Drawing.Point(50, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1029, 386);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nombre_ua
+            // 
+            this.nombre_ua.HeaderText = "Nombre de las unidades administrativas";
+            this.nombre_ua.Name = "nombre_ua";
+            // 
+            // empleo_anterior
+            // 
+            this.empleo_anterior.HeaderText = "Empleo anterior";
+            this.empleo_anterior.Name = "empleo_anterior";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtRutaDirectorio);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1211, 598);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "boton que abre un directorio";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Elegir directorio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtRutaDirectorio
+            // 
+            this.txtRutaDirectorio.Location = new System.Drawing.Point(190, 103);
+            this.txtRutaDirectorio.Name = "txtRutaDirectorio";
+            this.txtRutaDirectorio.Size = new System.Drawing.Size(428, 20);
+            this.txtRutaDirectorio.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 708);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre_ua;
+        private System.Windows.Forms.DataGridViewComboBoxColumn empleo_anterior;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRutaDirectorio;
+    }
+}
+
---
+++ Desarrollo_C#/Desarrollo_C#/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desarrollo_C_
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            basedatosSQLite();
+            ComboBoxLenguaIndigena();
+        }
+
+        private void basedatosSQLite()
+        {
+            string cadena = "Data Source = Desarrollo_CSharp.db; Version=3;"
+                ;
+            using (SQLiteConnection conn = new SQLiteConnection(cadena))
+            {
+                conn.Open();
+                string query = "SELECT unidad_administrativa FROM TR_UA";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string nombreComision = reader["unidad_administrativa"].ToString();
+                            dataGridView1.Rows.Add(nombreComision);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ComboBoxLenguaIndigena()
+        {
+            // Crear una lista para almacenar los datos del ComboBox
+            List<string> opcionesComboBox = new List<string>();
+
+            string connectionString = "Data Source = Desarrollo_CSharp.db; Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT id_lengua_indigena FROM TC_LENGUA_INDIGENA";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            opcionesComboBox.Add(reader["id_lengua_indigena"].ToString());
+                        }
+                    }
+                }
+            }
+
+            // Asignar ComboBox a cada celda de la columna c2
+            foreach (DataGridViewRow fila in dataGridView1.Rows)
+            {
+                DataGridViewComboBoxCell comboBoxCell = new DataGridViewComboBoxCell();
+
+                // Añadir las opciones al ComboBoxCell
+                comboBoxCell.Items.AddRange(opcionesComboBox.ToArray());
+
+                // Añadir el ComboBoxCell a la columna c2 de la fila actual
+                fila.Cells["empleo_anterior"] = comboBoxCell;
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del FolderBrowserDialog
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                // Título del diálogo
+                folderDialog.Description = "Seleccione un directorio";
+
+                // Mostrar el diálogo y verificar si el usuario seleccionó un directorio
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Obtener la ruta del directorio seleccionado
+                    string rutaSeleccionada = folderDialog.SelectedPath;
+
+                    // Mostrar la ruta seleccionada en un control, como un TextBox
+                    txtRutaDirectorio.Text = rutaSeleccionada;
+
+                    // Puedes hacer algo con la ruta seleccionada aquí
+                    MessageBox.Show("Directorio seleccionado: " + rutaSeleccionada);
+                }
+            }
+        }
+    }
+}
